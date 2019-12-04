34b42db0a8e026853dcdc70c12d7d386d876fa66
sierrabis <55862444+sierrabis@users.noreply.github.com>
2019-12-04 11:52:42 -0500

    Add files via upload

--- java/java.js
+++ java/java.js
@@ -21,29 +21,13 @@ window.onload = function(){
 	  $(this).attr('src', $(this).data("src"));
 	});
 
-	$(".image-container").mouseover(function () {
-	  $(this).attr('src', $(this).data("hover"));
-	}).mouseout(function () {
-	  $(this).attr('src', $(this).data("src"));
-	});
-
-	$(".image-container").mouseover(function () {
-	  $(this).attr('src', $(this).data("hover"));
-	}).mouseout(function () {
-	  $(this).attr('src', $(this).data("src"));
-	});
-
-	$(".image-container").mouseover(function () {
-	  $(this).attr('src', $(this).data("hover"));
-	}).mouseout(function () {
-	  $(this).attr('src', $(this).data("src"));
-	});
-
-	$(".image-container").mouseover(function () {
-	  $(this).attr('src', $(this).data("hover"));
-	}).mouseout(function () {
-	  $(this).attr('src', $(this).data("src"));
-	});
+$('.skewable').mouseover(function(){
+		var skewX = Math.random()*60;
+		var skewY = Math.random()*60;
+		$(this).css('transform', 'skew('+skewX+'deg, '+skewY+'deg)');
+	}).mouseout(function(){
+		$(this).css('transform', 'skew(10deg, 10deg)');
+	})
 
 $("#orange").mouseover(function() {
     $(this).animate({ marginTop: 100 });
@@ -62,14 +46,6 @@ $("#green").mouseover(function() {
 
 $("#green").mouseout(function() {
     $(this).animate({ marginTop: -100 });
-});
-
-$("#red").mouseover(function() {
-    $(this).animate({ marginTop: 200 });
-});
-
-$("#red").mouseout(function() {
-    $(this).animate({ marginTop: 0 });
 });
 
 }
